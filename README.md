495131453d7821f1a7a163450c34d0961d4bf670
takaiyuk <takaishikawa1225@gmail.com>
2021-07-29 18:17:51 +0900

    Add notebooks using dino

--- README.md
+++ README.md
@@ -13,6 +13,8 @@ root@xxxxx:/workspace# venv-acitvate
 
 Name | Public Score | Private Score
 -- | -- | --
+submission_exp011.csv | 0.7004 | 0.6909
+submission_exp010.csv | 0.8013 | 0.8045
 submission_exp009.csv | 0.7772 | 0.7709
 submission_exp008.csv | 0.7801 | 0.7695
 submission_exp005.csv | 0.7909 | 0.7892
--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -5,4 +5,4 @@ if [[ $PORT == "" ]]; then
   PORT="8888"
 fi
 echo "PORT: ${PORT}"
-docker run --runtime=nvidia -it -d --name ${CONTAINER_NAME} -p 8501:8501 -p ${PORT}:${PORT} -v ${PWD}:/workspace -v ${HOME}/.kaggle:/root/.kaggle/ takaiyuk/ml-multimodal-ja-gpu:latest
+docker run --runtime=nvidia -it -d --name ${CONTAINER_NAME} --shm-size=2048m -p 8501:8501 -p ${PORT}:${PORT} -v ${PWD}:/workspace -v ${HOME}/.kaggle:/root/.kaggle/ takaiyuk/ml-multimodal-ja-gpu:latest
